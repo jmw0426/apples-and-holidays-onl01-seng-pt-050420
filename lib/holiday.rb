138abc18c02309337e93c09685896149f5d6c821
Joshua Williams <jmw0426@hotmail.com>
2020-05-15 17:53:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,7 +85,6 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
-      holiday
     end
   end.flatten.compact
 end
